af2d8b7ebf056664a832d2d92e37ac237db75bfc
gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2019-02-03 19:18:07 +0530

    hunterset4

--- hunter/set4/33.py
+++ hunter/set4/33.py
@@ -7,19 +7,27 @@ for i in range(n):
     if l[i]=='a':
         if l[i+1]=='b':
             c+=1
-        else:  
+        if l[i+1]=='a':  
             c+=1
             ll.append(c)
             c=0
+        else:
+            ll.append(c)
+            c=0
+
         
     if l[i]=='b': 
         if l[i+1]=='a':
             c+=1 
             if l[i-1]=='b':
                 c=0
-        else:   
+        if l[i+1]=='b':  
             c+=1
             ll.append(c) 
             c=0
+        else:
+            ll.append(c) 
+            c=0
+
 ll.append(c) 
 print(max(ll))
--- hunter/set4/38.py
+++ hunter/set4/38.py
@@ -5,8 +5,8 @@ ll=[]
 o=0
 flag=0
 for i in permutations(l,n*2):
-    # if i[-1]=='{':
-    #     break
+    if i[-1]=='{':
+        break
     if i[0]=='}':
         break
     for j in i:
