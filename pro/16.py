0ce36646f8bd846d57af9feb5ac7501b6e7a9d72
gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2019-02-22 00:35:26 +0530

    pro

--- pro/16.py
+++ pro/16.py
@@ -1,5 +1,7 @@
 print("ddddddd")
 print("ddddddd")
+
+print("ddddddd")
 print("ddddddd")
 print("ddddddd")
 print("ddddddd")
--- pro/17.py
+++ pro/17.py
@@ -1,1 +1,7 @@
 
+print("ddddddd")
+print("ddddddd")
+print("ddddd")
+print("ddddd")
+print("ddddd")
+print("ddddd")
