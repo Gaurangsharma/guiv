acead57d2e3ac84632adceafe86f57a9828ca9bd
Gaurangsharma <gaurang.sharma_cs16@gla.ac.in>
2020-09-15 23:15:27 +0530

    hack it

--- codechef/hack.py
+++ codechef/hack.py
@@ -1,7 +1,7 @@
 t=int(input())
 if(t>=1 and t<=10**3):
     for _ in range(t):
-        n=list(input())
-        for i in range(len(n)):
-            print(int(n[i])-2,sep="",end="")
+        n=int(input())
+        for i in str(n):
+            print(int(i)-2,sep="",end="")
         print()
